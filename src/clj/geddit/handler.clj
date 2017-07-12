a626718a9165ae6ca00156dd7a0fa0a67ad16b58
Glenn <gleenn@gmail.com>
2017-07-12 14:30:11 -0400

    add silly route

--- src/clj/geddit/handler.clj
+++ src/clj/geddit/handler.clj
@@ -1,12 +1,13 @@
 (ns geddit.handler
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [resource-response]]
+            [ring.util.response :refer [response resource-response]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (defroutes routes
-  (GET "/" [] (resource-response "index.html" {:root "public"}))
-  (resources "/"))
+           (GET "/" [] (resource-response "index.html" {:root "public"}))
+           (GET "/news" [] (response "yo what's up"))
+           (resources "/"))
 
 (def dev-handler (-> #'routes wrap-reload))
 
